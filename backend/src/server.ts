96ed2e9cb223e76c31170ebc8d158244db3e43fc
Harish Nigam <harishnigam21@gmail.com>
2026-04-04 10:33:51 +0530

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,5 @@
+import "dotenv/config";
 import app from "./app";
-
 const PORT = process.env.PORT || 3000;
 app.get("/", (req, res) =>
   res.status(200).json({ message: "Backend Server running successfully" }),
